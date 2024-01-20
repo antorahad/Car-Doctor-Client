eb23abd6625877b9e6d8e2ffd36d369e1094c7e1
antorahad <antorahad6@gmail.com>
2024-01-20 12:26:22 +0600

    checkout form

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,7 +1,8 @@
 import { FaArrowRightLong } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Service = ({ service }) => {
-    const { title, img, price } = service
+    const { _id, title, img, price } = service
     return (
         <div>
             <div className="card bg-base-50 shadow-md rounded-md">
@@ -12,7 +13,9 @@ const Service = ({ service }) => {
                     <h2 className="card-title text-[25px] font-[700]">{title}</h2>
                     <div className="card-actions items-center justify-between">
                         <p className="text-orange-500 text-[20px] font-[600]">Price: ${price}</p>
-                        <button className="bg-none text-orange-500 border-none outline-none"><FaArrowRightLong size={20}></FaArrowRightLong></button>
+                        <Link to={`/checkout/${_id}`}>
+                             <button className="bg-none text-orange-500 border-none outline-none"><FaArrowRightLong size={20}></FaArrowRightLong></button>
+                        </Link>
                     </div>
                 </div>
             </div>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import Layout from "../Layout";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import Checkout from "../pages/Checkout";
 
 const router = createBrowserRouter([
     {
@@ -20,6 +21,11 @@ const router = createBrowserRouter([
         {
           path: 'signup',
           element: <Signup/>
+        },
+        {
+          path: 'checkout/:id',
+          element: <Checkout/>,
+          loader: ({params}) => fetch(`http://localhost:4000/services/${params.id}`)
         }
       ]
     },
